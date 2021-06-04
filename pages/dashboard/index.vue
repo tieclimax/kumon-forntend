90c6161203a8787326e354761914da3af05fbc62
tieclimax <60050231@kmitl.ac.th>
2021-06-04 18:50:30 +0700

    first push

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">User Information</h1>
+
+      <b-card class="mt-3" header="Form Data Result">
+        <p>ชื่อ สกุล : {{ $auth.user.insname_th }}</p>
+        <p>Fullname : {{ $auth.user.insname_en }}</p>
+        <p>username : {{ $auth.user.username }}</p>
+        <p>บทบาท : {{ $auth.user.statusname }}</p>
+      </b-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: ["auth"],
+  data() {
+    return {
+      user: this.$auth.user,
+    };
+  },
+};
+</script>
+
+
